3a023580b4e157ec4df7a0ff548038e809ab8391
afonseca00 <alexandref.rodrigues8@gmail.com>
2022-12-15 07:42:18 +0000

    Libft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: afonseca <afonseca@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 15:59:18 by afonseca          #+#    #+#             */
-/*   Updated: 2022/12/14 23:00:33 by afonseca         ###   ########.fr       */
+/*   Updated: 2022/12/15 07:42:05 by afonseca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (ft_substr(s1, 0, i + 1));
 }
 
-
+/*
 int main()
 {
    printf("%s\n", ft_strtrim("aaola  alexaa","a"));
 }
-
+*/
 //vai se dar um separador e a funcao vai eliminar 
 //no inicio e no final
